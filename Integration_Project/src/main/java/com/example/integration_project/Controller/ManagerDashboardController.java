77d0b9d610dd0709b0f12b3063cf53fdec94c490
DellDinde <delldinde8@gmail.com>
2025-11-22 16:22:25 -0500

    Added ManagerDashboardController.

--- Integration_Project/src/main/java/com/example/integration_project/Controller/ManagerDashboardController.java
+++ Integration_Project/src/main/java/com/example/integration_project/Controller/ManagerDashboardController.java
@@ -1,5 +1,15 @@
 package com.example.integration_project.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ManagerDashboardController {
 
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
 }
