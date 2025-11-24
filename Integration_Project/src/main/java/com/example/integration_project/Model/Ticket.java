ebbf2041ec5fdf48848513736524ff03415f0932
Emmanuelle <EmmanuelleIsis.Mbeug@stu.crc-lennox.qc.ca>
2025-11-24 17:58:46 -0500

    Implementation of the Ticket.java class

--- Integration_Project/src/main/java/com/example/integration_project/Model/Ticket.java
+++ Integration_Project/src/main/java/com/example/integration_project/Model/Ticket.java
@@ -1,4 +1,31 @@
 package com.example.integration_project.Model;
 
 public class Ticket {
+    private final String aTicketID;
+    private final String aMovieName;
+    private final String aShowtime;
+
+
+    public Ticket(String pTicketId, String pMovieName, String pShowtime) {
+        aTicketID = pTicketId;
+        aMovieName = pMovieName;
+        aShowtime = pShowtime;
+    }
+
+    public String getaTicketID() {
+        return aTicketID;
+    }
+
+    public String getaMovieName() {
+        return aMovieName;
+    }
+
+    public String getaShowtime() {
+        return aShowtime;
+    }
+
+    public String toString() {
+        return "Ticket Confirmation\n" + "Ticket ID: " + aTicketID + "\n" +
+                "Movie: " + aMovieName + "\n" + "Showtime: " + aShowtime + "\n";
+    }
 }
