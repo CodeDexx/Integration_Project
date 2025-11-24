66a414e09a5c8e237c37e15da57ddfb4f39396f6
DellDinde <delldinde8@gmail.com>
2025-11-24 03:29:04 -0500

    gitignore

--- Integration_Project/src/main/java/com/example/integration_project/Model/Client.java
+++ Integration_Project/src/main/java/com/example/integration_project/Model/Client.java
@@ -1,8 +1,4 @@
 package com.example.integration_project.Model;
 
-public class Client extends User{
-
-    public Client(String pName, String pEmail, String pPassword) {
-        super(pName, pEmail, pPassword);
-    }
+public class Client {
 }
