123cae9ee34790026ad06aadcdad31dfbb96868a
DellDinde <delldinde8@gmail.com>
2025-11-30 22:34:44 -0500

    MovieTheaterApplication.java forced commit.

--- Integration_Project/src/main/java/com/example/integration_project/MovieTheaterApplication.java
+++ Integration_Project/src/main/java/com/example/integration_project/MovieTheaterApplication.java
@@ -10,7 +10,11 @@ import java.io.IOException;
 public class MovieTheaterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+<<<<<<< HEAD
         FXMLLoader fxmlLoader = new FXMLLoader(MovieTheaterApplication.class.getResource("login-view.fxml"));
+=======
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+>>>>>>> b7c7ea1 (MovieTheaterApplication.java forced commit.)
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Login/Sign Up");
         stage.setScene(scene);
